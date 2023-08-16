eded1464debc058905f6d9c79cd88e10eb463225
kumarkasaramoni03 <kumarkasaramoni03@gmail.com>
2023-08-16 03:36:59 -0700

    updated the src file

--- microservice-updates/order/src/main/java/com/programmingtechi/orderservice/OrderServiceApplication.java
+++ microservice-updates/order/src/main/java/com/programmingtechi/orderservice/OrderServiceApplication.java
@@ -1,8 +1,13 @@
 package com.programmingtechi.orderservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class OrderServiceApplication {
 
